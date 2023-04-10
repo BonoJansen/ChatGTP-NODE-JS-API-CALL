68c8fcde46d816906119dd978cc301fde8ee6a4a
BonoJansen <91391274+BonoJansen@users.noreply.github.com>
2023-04-10 03:20:51 +0200

    removed random extra questions

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ async function chatGTP(input, apiKey) {
         let data = JSON.stringify({
             model: "gpt-3.5-turbo",
             messages: [
-                { "role": "system", "content": "You are a helpful assistant." },
-                { "role": "user", "content": "Who won the world series in 2020?" },
-                { "role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020." },
+//                 { "role": "system", "content": "You are a helpful assistant." },
+//                 { "role": "user", "content": "Who won the world series in 2020?" },
+//                 { "role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020." },
                 { "role": "user", "content": input }
             ]
         });
